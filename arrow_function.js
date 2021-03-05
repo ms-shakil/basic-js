7ecf0afe1fcd562b1523647a7374c3ea714f1bca
Sahabuddin Shakil <mdshakil8796@gmail.com>
2021-03-05 16:19:28 +0600

    some work with js

---
+++ arrow_function.js
@@ -0,0 +1,50 @@
+// // 1st example
+// const firstFunction =() =>{
+//     // console.log("my first arrow function")
+// }
+// firstFunction()
+// // 2nd example 
+// const add = (a,b) =>{
+//     return a+b
+// }
+// // console.log(add(3,5))
+
+// use map and filter inside arrow function
+var studentData =[
+    {
+        id:2323,    
+        name:"shakil",
+        gpa:2.50,
+        district:"comilla"},
+    {
+        id:3534,
+        name:"payer",
+        gpa:3.00,
+        district:"comilla"
+        
+    },
+    {
+        id:354444,
+        name:"asif",
+        gpa:3.50,
+        district:"dhaka"
+        
+    },
+    {
+        id:3134,
+        name:"fahad",
+        gpa:4.00,
+        district:"comilla"
+        
+    }
+]
+
+const find_StudentName =() =>{
+    return studentData.filter(function(x){
+        return x.gpa > 3.00;
+    }).map(function(y){
+        return y.name
+    })
+}
+
+console.log(find_StudentName())
