2e7f3b83df3e00458e96230607fbbf153010b496
ms-shakil <mdshakil8796@gmail.com>
2024-01-26 01:29:28 +0600

    about forEach Fun

--- Array.js
+++ Array.js
@@ -9,3 +9,17 @@ fruits[1]= "orange....."
 for(let i =0 ;i< fruits.length;i++){
     console.log(fruits[i])
 }
+
+
+//////// array Iteration
+
+let Number =[42,12,17,88,23,54]
+
+// for eathch
+function MyFun(value,index,array){
+    console.log(value)
+    console.log(index)
+    console.log(array)
+    console.log("_________")
+}
+Number.forEach(MyFun)
