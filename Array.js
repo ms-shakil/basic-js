c2196a9d5bb08de69b6a3f484cb09f62ed08b756
ms-shakil <mdshakil8796@gmail.com>
2024-01-26 03:19:17 +0600

    array iteration

--- Array.js
+++ Array.js
@@ -13,7 +13,7 @@ for(let i =0 ;i< fruits.length;i++){
 
 //////// array Iteration
 
-let Number =[42,12,17,88,23,54]
+const Number =[42,12,17,88,23,54]
 
 // for eathch
 function MyFun(value,index,array){
@@ -23,3 +23,24 @@ function MyFun(value,index,array){
     console.log("_________")
 }
 Number.forEach(MyFun)
+
+
+function Map_Fun(value){ // map return some thing
+    return value*2
+}
+const NewNum =Number.map(Map_Fun)
+console.log(NewNum)
+
+
+
+function Filter_Fun(value){
+    if(value >30){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+const NewFilterArry = Number.filter(Filter_Fun)
+console.log(NewFilterArry)
