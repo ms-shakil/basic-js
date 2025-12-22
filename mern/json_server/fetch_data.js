6e9df42ac75c3d0409b229cedbba9dc9532ba412
ms-shakil <mdshakil8796@gmail.com>
2025-12-22 23:46:47 +0600

    about basic json server

---
+++ mern/json_server/fetch_data.js
@@ -0,0 +1,37 @@
+
+let apiUrl ="http://localhost:3001/books"
+
+let newData ={
+     "titel":"book2",
+     "author":"osman"
+}
+let updateData ={
+    
+     "titel":"javascipt ",
+     "author":"osman hadi"
+     
+}
+let callApi = async ()=>{
+    let response = await fetch(apiUrl)
+    let data = await response.json()
+    console.log(data)
+}
+callApi()
+
+async function addData() {
+    await fetch(apiUrl,{
+    method:"POST",
+    body:JSON.stringify(newData)
+})
+}
+// addData()
+
+
+async function update() {
+    let url = `http://localhost:3001/books/${1}`
+    await fetch(url,{
+    method:"PUT",
+    body:JSON.stringify(updateData)
+})
+}
+update()
