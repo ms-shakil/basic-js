d7ce1acbbd39215590ebd57a1455d5e6b0ad0122
ms-shakil <mdshakil8796@gmail.com>
2025-12-23 20:26:25 +0600

    static in class

--- mern/OOP/exchangeRate.js
+++ mern/OOP/exchangeRate.js
@@ -17,3 +17,23 @@ class ExchangeRate {
   const value = await rate.getRate("usd", "bdt")
   console.log(value)
 })()
+
+
+// static
+class ExchangeRate2 {
+      static apiUrl ="https://api.exchangerate-api.com/v4/latest/"
+     static async getRate(c1='usd',c2){
+          const url = this.apiUrl+c1
+          const response = await fetch(url)
+          const data = await response.json()
+          const rate = data.rates[c2.toUpperCase()]
+          return rate
+
+     }
+}
+
+(async () => {
+  
+  const value = await ExchangeRate2.getRate("usd", "bdt") // static use korle object er moto call korajai
+  console.log(value)
+})()
